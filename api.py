f3e686591cea8ccaa9942d61c6a977d349f193d4
Marccb18 <146984794+Marccb18@users.noreply.github.com>
2024-04-28 09:48:07 +0200

    Fer un ingres ja funciona

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template,redirect, url_for, flash
 from flask_restful import Resource, Api, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -11,8 +11,9 @@ api = Api(app)
 url = 'http://127.0.0.1:5000/transactions'
 
 # Configuración de la base de datos SQLite utilizando SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bank.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = '1234'
 db = SQLAlchemy(app)
 
 # Modelo de datos para la tabla 'bank'
@@ -50,7 +51,7 @@ class Bank(Resource):
         descripcion = args['descripcion']
         new_transaction = BankModel(concepto=concepto, cantidad=cantidad, fecha=fecha, descripcion=descripcion)
         db.session.add(new_transaction)
-        db.session.commit()
+        db.session.commit()  # Commit the transaction
         return {'id': new_transaction.id, 'concepto': concepto, 'cantidad': cantidad,
                 'fecha': fecha.strftime('%Y-%m-%d'), 'descripcion': descripcion}, 201
 
@@ -88,7 +89,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/new_transaction', methods=['GET','POST'])
+@app.route('/new_transaction', methods=['GET', 'POST'])
 def transaction():
     form = IngresoForm()
     if form.validate_on_submit():
@@ -99,11 +100,19 @@ def transaction():
         data = {
             'concepto': concepto,
             'cantidad': cantidad,
-            'fecha': fecha,
+            'fecha': fecha.strftime('%Y-%m-%d'),
             'descripcion': descripcion
         }
+        try:
+            response = requests.post(url, json=data)
+            if response.status_code == 201:
+                return redirect(url_for('index'))
+            else:
+                flash('Error al crear la transacción', 'error')
+        except requests.exceptions.RequestException as e:
+            flash('Error al enviar la solicitud', 'error')
     
-    return render_template('transaction.html')
+    return render_template('transaction.html', form=form)
 
 if __name__ == '__main__':
     with app.app_context():
