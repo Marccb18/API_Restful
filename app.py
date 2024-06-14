b5f4afe966ef538a9966d803e7854b64271d57b7
Marc Colom <marc18cb@gmail.com>
2024-06-14 19:02:03 +0200

    He de millorar sa estetica

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 from forms import IngresoForm, DeudaForm
 from models import db, Deuda,Transaccion
 from api import *
@@ -17,6 +17,44 @@ db.init_app(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/nueva_transaccion', methods=['GET','POST'])
+def nueva_transaccion():
+    form = IngresoForm()
+    if form.validate_on_submit():
+        concepto = form.concepto.data
+        cantidad = form.cantidad.data
+        fecha = form.fecha.data
+        descripcion = form.descripcion.data
+        if cantidad < 0:
+            es_gasto = True
+        else:
+            es_gasto = False
+        nueva_transaccion = Transaccion(concepto=concepto,cantidad=cantidad,fecha=fecha,descripcion=descripcion, es_gasto=es_gasto)
+        db.session.add(nueva_transaccion)
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        return render_template('transaction.html', form=form)
+
+@app.route('/nueva_deuda', methods=['GET','POST'])
+def nueva_deuda():
+    form = DeudaForm()
+    if form.validate_on_submit():
+        concepto = form.concepto.data
+        cantidad = form.cantidad.data
+        deudor = form.deudor.data
+        fecha = form.fecha.data
+        comentario = form.comentario.data
+        pagada = form.pagada.data
+        nueva_deuda = Deuda(concepto=concepto,cantidad=cantidad,deudor=deudor,fecha=fecha,comentario=comentario,pagada=pagada)
+        db.session.add(nueva_deuda)
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        return render_template('nueva_deuda.html', form=form)
+
+
 
 if __name__ == '__main__':
     with app.app_context():
